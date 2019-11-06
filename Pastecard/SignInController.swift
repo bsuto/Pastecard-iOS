acb580440dafd448b495609a3a932a90cb3fbd52
bsuto <brian.sutorius@gmail.com>
2019-11-06 09:43:30 -0500

    prevent swipe to dismiss sign in

--- Pastecard/SignInController.swift
+++ Pastecard/SignInController.swift
@@ -149,6 +149,11 @@ class SignInController: UIViewController {
     
     override func viewDidLoad() {
         
+        // prevent swipe to dismiss
+        if #available(iOS 13.0, *) {
+            self.isModalInPresentation = true
+        }
+        
         // assemble the sign up alert box
         alertBox.addTextField { (textField) -> Void in
             self.nameField = textField
