de51b7f96af627595989ddc3b98f82d12c8ab500
Brian Sutorius <brian.sutorius@gmail.com>
2023-04-22 10:56:54 -0400

    upgrade share extension to async await

--- PCShareExt/ShareViewController.swift
+++ PCShareExt/ShareViewController.swift
@@ -7,9 +7,12 @@
 
 import SwiftUI
 
+enum NetworkError: Error {
+    case appendError
+}
+
 @objc(ShareViewController)
 class ShareViewController: UIViewController {
-        
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .clear
@@ -32,13 +35,19 @@ class ShareViewController: UIViewController {
             if attachment.hasItemConformingToTypeIdentifier(textType) {
                 attachment.loadItem(forTypeIdentifier: textType, options: nil, completionHandler: { (data, error) in
                     if let sharedText = data as! String? {
-                        self.append(sharedText)
+                        Task {
+                            do { try await self.append(sharedText) }
+                            catch { return }
+                        }
                     }
                 })
             } else if attachment.hasItemConformingToTypeIdentifier(urlType) {
                 attachment.loadItem(forTypeIdentifier: urlType, options: nil, completionHandler: { (data, error) in
                     if let sharedURL = data as! URL? {
-                        self.append(sharedURL.absoluteString)
+                        Task {
+                            do { try await self.append(sharedURL.absoluteString) }
+                            catch { return }
+                        }
                     }
                 })
             }
@@ -60,7 +69,7 @@ class ShareViewController: UIViewController {
         ])
     }
     
-    private func append(_ text: String) {
+    private func append(_ text: String) async throws {
         let uid = UserDefaults(suiteName: "group.net.pastecard")!.string(forKey: "ID")
         let sendText = text.addingPercentEncoding(withAllowedCharacters: .alphanumerics) ?? ""
         
@@ -74,15 +83,17 @@ class ShareViewController: UIViewController {
             var request = URLRequest(url: url)
             request.httpMethod = "POST"
             request.httpBody = postData.data(using: String.Encoding.utf8)
-            // request.timeoutInterval = 5.0
+            request.timeoutInterval = 5.0
             
-            let task = URLSession.shared.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
-                // pause a moment so the Saving message is readable
-                DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(500)) {
-                    self.hideView()
-                }
+            let (_, response) = try await URLSession.shared.data(for: request)
+            guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+                throw NetworkError.appendError
             }
-            task.resume()
+            
+            // pause a moment so the Saving message is readable
+            DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(750)) {
+                self.hideView()
+            }
         }
     }
     
