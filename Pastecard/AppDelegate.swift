cab1a7a5a3f7381477bf8645b97fcbc81f637086
Brian Sutorius <brian.sutorius@gmail.com>
2023-10-26 14:18:20 -0400

    fix indentation

--- Pastecard/AppDelegate.swift
+++ Pastecard/AppDelegate.swift
@@ -9,60 +9,60 @@
 import UIKit
 
 enum ActionType: String {
-  case swapIcon = "swapIcon"
+    case swapIcon = "swapIcon"
 }
 
 enum Action: Equatable {
-  case swapIcon
-
-  init?(shortcutItem: UIApplicationShortcutItem) {
-    guard let type = ActionType(rawValue: shortcutItem.type) else {
-      return nil
+    case swapIcon
+    
+    init?(shortcutItem: UIApplicationShortcutItem) {
+        guard let type = ActionType(rawValue: shortcutItem.type) else {
+            return nil
+        }
+        
+        switch type {
+        case .swapIcon:
+            self = .swapIcon
+        }
     }
-
-    switch type {
-    case .swapIcon:
-      self = .swapIcon
-    }
-  }
 }
 
 class ActionService: ObservableObject {
-  static let shared = ActionService()
-  @Published var action: Action?
+    static let shared = ActionService()
+    @Published var action: Action?
 }
 
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-  private let actionService = ActionService.shared
-
-  func application(
-    _ application: UIApplication,
-    configurationForConnecting connectingSceneSession: UISceneSession,
-    options: UIScene.ConnectionOptions
-  ) -> UISceneConfiguration {
-    if let shortcutItem = options.shortcutItem {
-      actionService.action = Action(shortcutItem: shortcutItem)
+    private let actionService = ActionService.shared
+    
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        if let shortcutItem = options.shortcutItem {
+            actionService.action = Action(shortcutItem: shortcutItem)
+        }
+        
+        let configuration = UISceneConfiguration(
+            name: connectingSceneSession.configuration.name,
+            sessionRole: connectingSceneSession.role
+        )
+        configuration.delegateClass = SceneDelegate.self
+        return configuration
     }
-
-    let configuration = UISceneConfiguration(
-      name: connectingSceneSession.configuration.name,
-      sessionRole: connectingSceneSession.role
-    )
-    configuration.delegateClass = SceneDelegate.self
-    return configuration
-  }
 }
 
 class SceneDelegate: NSObject, UIWindowSceneDelegate {
-  private let actionService = ActionService.shared
-
-  func windowScene(
-    _ windowScene: UIWindowScene,
-    performActionFor shortcutItem: UIApplicationShortcutItem,
-    completionHandler: @escaping (Bool) -> Void
-  ) {
-    actionService.action = Action(shortcutItem: shortcutItem)
-    completionHandler(true)
-  }
+    private let actionService = ActionService.shared
+    
+    func windowScene(
+        _ windowScene: UIWindowScene,
+        performActionFor shortcutItem: UIApplicationShortcutItem,
+        completionHandler: @escaping (Bool) -> Void
+    ) {
+        actionService.action = Action(shortcutItem: shortcutItem)
+        completionHandler(true)
+    }
 }
