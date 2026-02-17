cf95373715fd0d5b2b8d602cb996f1ebd500a561
Brian Sutorius <brian.sutorius@gmail.com>
2026-02-16 19:17:56 -0500

    consolidate siri

--- Pastecard/AppIntents.swift
+++ Pastecard/AppIntents.swift
@@ -9,64 +9,83 @@ import AppIntents
 import PastecardCore
 
 struct GetText: AppIntent {
-    static var title: LocalizedStringResource = "Pastecard text"
-    static var description =
-    IntentDescription("Returns the text on your Pastecard.")
+    static var title: LocalizedStringResource = "Get Pastecard text"
+    static var description = IntentDescription("Returns the text on your Pastecard.")
     static var openAppWhenRun = false
     
     private let core = PastecardCore.shared
     
     private func loadText() async throws -> String {
-        if !core.isSignedIn {
-            return "Please sign in first."
-        }
-        
         let localText = core.loadLocal()
         if !localText.isEmpty {
             return localText
         } else {
-            // If local is empty, try to load from remote
+            // If local is empty, try to load from remote just in case
             return try await core.loadRemote()
         }
     }
     
     func perform() async throws -> some ReturnsValue<String> & ProvidesDialog {
-        let cardText = try await loadText()
-        let dialog = IntentDialog(stringLiteral: cardText)
-        return .result(value: cardText, dialog: dialog)
-    }
+            if !core.isSignedIn {
+                let dialog = IntentDialog("Please open the app and sign in first.")
+                return .result(value: "", dialog: dialog)
+            }
+            
+            let cardText = try await loadText()
+            let dialog: IntentDialog
+            if cardText.isEmpty {
+                dialog = "Your Pastecard is empty."
+            } else {
+                dialog = IntentDialog(stringLiteral: cardText)
+            }
+            
+            return .result(value: cardText, dialog: dialog)
+        }
 }
 
 struct AppendText: AppIntent {
     static var title: LocalizedStringResource = "Add to Pastecard"
-    static var description =
-    IntentDescription("Append some text to the end of your Pastecard.")
+    static var description = IntentDescription("Append some text to the end of your Pastecard.")
     static var openAppWhenRun = false
     
-    @Parameter(title: "Text")
-    var text: String?
+    @Parameter(title: "Text", requestValueDialog: IntentDialog("What would you like to add?"))
+    var text: String
+    
     static var parameterSummary: some ParameterSummary {
         Summary("Add \(\.$text) to your Pastecard")
     }
     
     private let core = PastecardCore.shared
-    
-    private func append(_ newText: String) async throws -> String {
-        if !core.isSignedIn {
-            return "Please sign in first."
-        }
-        
-        try await core.append(newText)
-        return "Saved!"
-    }
-    
+
     func perform() async throws -> some ProvidesDialog {
-        guard let text = text else {
-            throw $text.needsValueError("What would you like to add to your Pastecard?")
+        if !core.isSignedIn {
+            return .result(dialog: "Please open the app and sign in first.")
         }
         
-        let dialogMessage = try await append(text)
-        let dialog = IntentDialog(stringLiteral: dialogMessage)
-        return .result(dialog: dialog)
+        try await core.append(text)
+        return .result(dialog: "")
     }
 }
+
+struct ShortcutsProvider: AppShortcutsProvider {
+    static var shortcutTileColor: ShortcutTileColor {
+        return .navy
+    }
+    
+    static var appShortcuts: [AppShortcut] {
+        AppShortcut(
+            intent: GetText(),
+            phrases: ["What's on my \(.applicationName)", "Read me my \(.applicationName)", "Get \(.applicationName)"],
+            shortTitle: "Get Text",
+            systemImageName: "inset.filled.topthird.square"
+        )
+        AppShortcut(
+            intent: AppendText(),
+            phrases: ["Add something to my \(.applicationName)", "Add to \(.applicationName)"],
+            shortTitle: "Add Text",
+            systemImageName: "plus.square"
+        )
+    }
+}
+
+struct IntentExtension: AppIntentsExtension {}
